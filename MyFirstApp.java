c1f034fd625070d9e2361fc44cfe2727678b70bc
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-08-13 22:47:13 +0600

    first commit

---
+++ MyFirstApp.java
@@ -0,0 +1,8 @@
+public class MyFirstApp{
+    public static void main(String[] args) {
+       System.out.println("Hello world!"); 
+       System.out.println("Hello world!"); 
+       System.out.println("Hello world!"); 
+       System.out.println("Hello world"); 
+    }
+}
