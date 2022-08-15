9966d138e4e71e04695bb9c2bd4acf3b5e030b17
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-08-15 22:11:06 +0600

    data type aded

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,8 +1,24 @@
 public class MyFirstApp{
     public static void main(String[] args) {
-       System.out.println("Hello world!"); 
-       System.out.println("Hello world!"); 
-       System.out.println("Hello world!"); 
-       System.out.println("Hello world"); 
+    //    integer - 0, 1212, 343, 3546 -> int
+    // floating point number - 3.23, 5.343
+    // character - 'a' 'b' 'g'
+    // string - "ASDJSDAS" "BALL" " i am student"
+    // int types
+    byte numberOfSeats = 2;
+    short numberOfGears = 23;
+    int milage = 400;
+    long registrationNumber = 343234325454543L;
+
+    // float
+    float myPIzza = 3.23F;
+    double myStudents = 233.324343;
+
+    // char
+    char myFavouriteLetter = 'G';
+
+    // String
+    String name = "asif";
+
     }
 }
