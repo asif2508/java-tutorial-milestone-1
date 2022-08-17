9bd568cf3ba1a9f48b2b82c5125f541fc6bb72e2
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-08-18 01:14:09 +0600

    control flow added again

--- ControlFlow.java
+++ ControlFlow.java
@@ -21,12 +21,22 @@ public class ControlFlow {
     //     System.out.println("The car is good");
     // }
     // sakib
-    String friendOne = "rahim";
-    String friendTwo = "sakib";
-    if(friendOne == "rahim" || friendTwo == "sakiba"){
-        System.out.println("come in");
-    }else{
-        System.out.println("get lost");
-    }
+    // String friendOne = "rahim";
+    // String friendTwo = "sakib";
+    // if(friendOne == "rahim" || friendTwo == "sakiba"){
+    //     System.out.println("come in");
+    // }else{
+    //     System.out.println("get lost");
+    // }
+    // rahim, karim, jack -> authorized
+    // age 18 -> minimum
+    // String name = "rahima";
+    // int age = 17;
+    // boolean isGoodPerson = false;
+    // if(name == "rahim" || age >18 || isGoodPerson == true){
+    //     System.out.println("bear kinte parbe");
+    // }else{
+    //     System.out.println("kinte parbe na");
+    // }
     }
 }
