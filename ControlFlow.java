8a6a0e529466850bd57da0dbcecbd5c688c96dec
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-08-19 23:13:06 +0600

    hello world

--- ControlFlow.java
+++ ControlFlow.java
@@ -38,5 +38,33 @@ public class ControlFlow {
     // }else{
     //     System.out.println("kinte parbe na");
     // }
+
+    // Grading system
+    // 80-100 > A+, 70-79 > A, 60-69 > A-, 50-59 > B, <50 > Fail
+    // int mark = 30;
+    // if(mark >= 80 && mark<=100){
+    //     System.out.println("A+");
+    // }else if(mark >= 70 && mark< 80){
+    //     System.out.println("A");
+    // }
+    // else if(mark >= 60 && mark< 70){
+    //     System.out.println("A-");
+    // }
+    // else if(mark >= 50 && mark< 60){
+    //     System.out.println("B");
+    // }else{
+    //     System.out.println("F");
+    // }
+    // int age = 32;
+    // if(age > 0 && age < 18){
+    //     System.out.println("Child");
+    // }else if(age > 19 && age < 30){
+    //     System.out.println("Men");
+    // }else if(age > 30 && age < 45){
+    //     System.out.println("middle aged");
+    // }else{
+    //     System.out.println("OLd");
+    // }
+    
     }
 }
---
+++ RealWorld.java
@@ -0,0 +1,16 @@
+public class RealWorld {
+    public static void main(String[] args) {
+        // area of triangle  =  .5 * W * L;
+        // area of square = Length^2 = Length * Length; 
+        // area of circle = 3.1416 * (radius * radius)
+        int land = 50000;
+        int height = 8000;
+        
+        double area = .5 * land * height;
+        System.out.println("The area is "+ area +" cm^2");
+
+        // int Length = 20;
+        // double area = Length * Length;
+
+    }
+}
